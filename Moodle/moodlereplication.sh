f4e3eaa287a8abd17f6e8b41bf65b8a3079091fb
asift91 <60592188+asift91@users.noreply.github.com>
2020-05-12 13:46:45 +0530

    initial commit for moodlereplication.sh

---
+++ Moodle/moodlereplication.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+change_location() {
+    echo "change locationfunction"
+    sudo mkdir /azlamp/html/${1}
+    sudo cp -rf /var/www/html/moodle/* /azlamp/html/${1}
+}
+configuring_certs() {
+    echo "certs func"
+    sudo mkdir /azlamp/certs/${1}
+    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /azlamp/certs/${1}/nginx.key -out /azlamp/certs/${1}/nginx.crt -subj "/C=US/ST=WA/L=Redmond/O=IT/CN=${1}"
+    sudo chown www-data:www-data /azlamp/certs/${1}/nginx.*
+    sudo chmod 400 /azlamp/certs/${1}/nginx.*
+
+}
+linking_data_location() {
+    echo "linking func"
+    sudo mkdir -p /azlamp/data/${1}/uploads
+    sudo ln -s /azlamp/data/${1}/uploads /azlamp/html/${1}/uploads
+    sudo chmod 0777 /azlamp/data/${1}/uploads
+}
+update_nginx_configuration() {
+    echo "update nginx"
+    cd /azlamp/bin/
+    sudo sed -i "s~#1)~1)~" /azlamp/bin/update-vmss-config
+    sudo sed -i "s~#    . /azlamp/bin/utils.sh~   . /azlamp/bin/utils.sh~" /azlamp/bin/update-vmss-config
+    sudo sed -i "s~#    reset_all_sites_on_vmss true VMSS~    reset_all_sites_on_vmss true VMSS~" /azlamp/bin/update-vmss-config
+    sudo sed -i "s~#;;~;;~" /azlamp/bin/update-vmss-config
+    echo "sleep for 30 seconds"
+    sleep 30
+}
+replication() {
+    echo "replication func"
+    cd /usr/local/bin/
+    sudo bash update_last_modified_time.azlamp.sh
+}
+
+# ${1} value is a domain name which will update in runtime
+change_location ${1}
+configuring_certs ${1}
+linking_data_location ${1}
+update_nginx_configuration
+replication
